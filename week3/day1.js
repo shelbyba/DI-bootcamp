8b42d60af988da7cb295923c34910229b20c7b01
shebli <drshebly28@gmail.com>
2022-11-07 11:28:42 +0200

    first commit

---
+++ week3/day1.js
@@ -0,0 +1,6 @@
+
+let sentence = ( ' hello man wasup' ) ; 
+console.log ( sentence);
+
+let sentenc = ( 'john is waking home') ;
+console.log (sentenc) ;
---
+++ week4/Domday1.js
@@ -0,0 +1,8 @@
+// 1.The div DOM node? 
+//1. The div DOM node?
+
+//2. The ul DOM node?
+
+console.log ( document.firstChild);
+
+
