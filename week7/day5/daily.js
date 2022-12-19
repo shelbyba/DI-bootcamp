ae13c57aba99610abaca8755e218ad8cd754d137
shebli <drshebly28@gmail.com>
2022-12-19 04:52:18 +0200

    new

---
+++ week7/day5/daily.js
@@ -0,0 +1,13 @@
+// let arr = [true,true,true];
+//  let even = arr.every( item => {
+//    return item === true;
+//  })
+//  console.log(even);
+
+ let arr = [2,1,0,11,];
+ let truth= arr.every( item => {
+   return item === 10;
+})
+console.log(truth);
+
+
